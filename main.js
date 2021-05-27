9526a6ded9989d3bb5ab1c4e76f6f42e9c724d38
Naimur53 <naimurrhman53@gmail.com>
2021-05-28 00:25:23 +0600

    max valule of array

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ else{
 }
 **/
 console.log(max);
+
+
+//arry max value
+var value = [3,533,34,2,524654564];
+var maxArry= value[0];
+for(var i=0;i<value.length;i++){
+    if(value[i]>maxArry){
+        maxArry=value[i];
+    }
+}
+console.log(maxArry);
